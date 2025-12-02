e24edb34400e4c157c4d74c46402f563132f38c6
Ian Ellis <ian.ellis@anz.com>
2025-12-03 08:42:02 +1100

    Add Open CV

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.internal.impldep.org.junit.experimental.categories.Categories.CategoryFilter.include
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,3 @@ dependencyResolutionManagement {
 
 rootProject.name = "Color Checker"
 include(":app")
- 
