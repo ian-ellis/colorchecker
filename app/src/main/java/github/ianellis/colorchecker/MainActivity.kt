015eec61458586243f5a39db315ebe019178de1d
Ian Ellis <ian.ellis@anz.com>
2025-12-03 23:15:13 +1100

    Update MainActivity to coordinate permissions, camera view management and object detection / overlays
    Pretty basic set up that could be improved by abstracting permission management into some kind of helper and updating state to use a view model. That being said viewmodels can get tricky with views like CameraBridgeViewBase that result in 2 way data transfers. ViewModels work much better with one way data flows. However this feels appropriate for the state of this small POC project

--- app/src/main/java/github/ianellis/colorchecker/MainActivity.kt
+++ app/src/main/java/github/ianellis/colorchecker/MainActivity.kt
@@ -1,47 +1,108 @@
 package github.ianellis.colorchecker
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import github.ianellis.colorchecker.ui.theme.ColorCheckerTheme
+import github.ianellis.colorchecker.matdecoration.ObjectDetectionMatDecorator
+import github.ianellis.colorchecker.objectdetection.ORBObjectDetector
+import org.opencv.android.CameraBridgeViewBase
+import org.opencv.android.OpenCVLoader
+import org.opencv.android.Utils
 
 class MainActivity : ComponentActivity() {
+
+    companion object {
+        const val CAMERA_PERMISSION_REQUEST_CODE: Int = 200
+    }
+
+    private lateinit var cameraManager: CameraViewManager
+
+    private lateinit var grantPermissionsView: View
+    private lateinit var grantPermissionsButton: Button
+    private lateinit var cameraView: CameraBridgeViewBase
+    private var permissionsGranted: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            ColorCheckerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+        if (!OpenCVLoader.initLocal()) {
+            (Toast.makeText(this, "OpenCV initialization failed!", Toast.LENGTH_LONG)).show();
+            return;
+        }
+        initView()
+        initCameraManager()
+    }
+
+    private fun initView() {
+        setContentView(R.layout.cameraview_layout)
+        grantPermissionsView = findViewById(R.id.grant_permissions_view)
+        grantPermissionsButton = findViewById(R.id.grant_permissions_button)
+        grantPermissionsButton.setOnClickListener { openPermissionSettings() }
+        cameraView = findViewById<View>(R.id.camera_view) as CameraBridgeViewBase
+    }
+
+    private fun initCameraManager() {
+        // load target image
+        val colorCheckerImage = Utils.loadResource(this, R.raw.reference)
+        //create detector
+        val objectDetector = ORBObjectDetector(colorCheckerImage)
+        // create decorator to draw box when matches found
+        val objectDetectionMatDecorator = ObjectDetectionMatDecorator(objectDetector)
+        // create camera manager to draw overlays
+        cameraManager = CameraViewManager(cameraView, objectDetectionMatDecorator)
+        // set lifecycle observer to manage view state
+        lifecycle.addObserver(cameraManager)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        grantPermissions()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String?>,
+        grantResults: IntArray,
+        deviceId: Int
+    ) {
+        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE && grantResults.firstOrNull() == PackageManager.PERMISSION_GRANTED) {
+            onCameraPermissionGranted()
+        } else {
+            grantPermissionsView.visibility = View.VISIBLE
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults, deviceId)
+    }
+
+    private fun onCameraPermissionGranted() {
+        grantPermissionsView.visibility = View.GONE
+        cameraManager.onPermissionsGranted()
+    }
+
+    private fun grantPermissions() {
+        permissionsGranted =
+            checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
+        if (!permissionsGranted) {
+            requestPermissions(
+                arrayOf(Manifest.permission.CAMERA),
+                CAMERA_PERMISSION_REQUEST_CODE
+            )
+
+        } else {
+            onCameraPermissionGranted()
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ColorCheckerTheme {
-        Greeting("Android")
+    private fun openPermissionSettings() {
+        startActivity(Intent().apply {
+            action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            data = Uri.fromParts("package", packageName, null)
+        })
     }
 }
